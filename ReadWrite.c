45948fea73071e398db0a1785108dda435b484b6
kaveO <alban.billiette@gmail.com>
2025-02-21 07:04:23 +0100

    + | Removed pragma ignore of the Unreferenced Parameter warning, now I will just use UNREFERENCED_PARAMETER, this is a better approach and way to code

--- ReadWrite.c
+++ ReadWrite.c
@@ -1,9 +1,10 @@
-#pragma warning (disable : 4100)
+#pragma once
 
 #include "ReadWrite.h"
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath)
 {
+	UNREFERENCED_PARAMETER(pRegistryPath);
 	pDriverObject->DriverUnload = UnloadDriver;
 	DbgPrintEx(0, 0, "Driver Entry -> Hello Kernel World");
 	return (STATUS_SUCCESS);
@@ -11,6 +12,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath
 
 NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverObject)
 {
+	UNREFERENCED_PARAMETER(pDriverObject);
 	DbgPrintEx(0, 0, "Driver Unload -> Goodbye Kernel World");
 	return (STATUS_SUCCESS);
 }
