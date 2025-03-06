3aa1616f6798f54297a41d7aea060d88659be5db
kaveO <alban.billiette@gmail.com>
2025-03-06 21:17:23 +0100

    + | New update, have to work on usermode communication !

--- ReadWrite.h
+++ ReadWrite.h
@@ -1,4 +1,6 @@
-#include "ntifs.h"
+#pragma once
+
+#include <ntifs.h>
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegistryPath);
 
--- communication.c
+++ communication.c
@@ -1,14 +1,33 @@
-#include "communication.h"
+#include "Communication.h"
+#include "Data.h"
 
 NTSTATUS IoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 {
-	Irp->IoStatus.Status = STATUS_SUCCESS;
-	Irp->IoStatus.Information = 0;
+	NTSTATUS Status = STATUS_UNSUCCESSFUL;
+	ULONG ByteIO = 0;
 
-	DbgPrintEx(0, 0, "IO Control function has been called !");
+	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
 
-	IoCompleteRequest(Irp, IO_NO_INCREMENT);
-	return (STATUS_SUCCESS);
+	ULONG ControlCode = stack->Parameters.DeviceIoControl.IoControlCode;
+
+	if (ControlCode == IOCTL_GET_CLIENT_ADDRESS)
+	{
+		PULONG Output = (PULONG)Irp->AssociatedIrp.SystemBuffer;
+		Output = assaultCubeDLLAddress;
+
+		DbgPrintEx(0, 0, "Client Address requested !\n");
+		Status = STATUS_SUCCESS;
+		ByteIO = sizeof(*Output);
+	}
+	else
+	{
+		ByteIO = 0;
+	}
+	Irp->IoStatus.Status = Status;
+	Irp->IoStatus.Information = ByteIO;
+	IofCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return (Status);
 }
 
 NTSTATUS CreateCall(PDEVICE_OBJECT DeviceObject, PIRP Irp)
